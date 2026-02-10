c5349138724baa7310eda39fc3c560a71905852d
phantekzy <mainilotfi@gmail.com>
2026-02-10 14:05:03 +0100

    working on the inputs

--- Main.java
+++ Main.java
@@ -6,6 +6,23 @@ public class Main {
     VaultManager myVault = new VaultManager();
     boolean running = true;
 
+    while (running) {
+      System.out.println("\n1. Add New Password");
+      System.out.println("2. View All Passwords");
+      System.out.println("3. Exit");
+      System.out.print("Choose an option: ");
+
+      int choice = scan.nextInt();
+      scan.nextLine();
+
+      if (choice == 1) {
+        System.out.print("Enter Website: ");
+        String site = scan.nextLine();
+        System.out.print("Enter Username: ");
+        String user = scan.nextLine();
+      }
+    }
+
     scan.close();
   }
 }
