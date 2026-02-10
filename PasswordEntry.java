6e4e77ed47334f8e2aacd7ae6559775df917865d
phantekzy <mainilotfi@gmail.com>
2026-02-10 13:45:53 +0100

    created the constructor

--- PasswordEntry.java
+++ PasswordEntry.java
@@ -2,4 +2,10 @@ public class PasswordEntry {
   private String website;
   private String username;
   private String password;
+
+  public PasswordEntry(String website, String username, String password) {
+    this.website = website;
+    this.username = username;
+    this.password = password;
+  }
 }
