08cde86c1ce457e2ee67689a98a316200a9377a7
phantekzy <mainilotfi@gmail.com>
2026-02-10 14:02:50 +0100

    im building the controler

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    VaultManager myVault = new VaultManager();
+    boolean running = true;
+
+    scan.close();
+  }
 }
