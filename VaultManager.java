fea1f1caa6817bcb1b04def3311936fcab9a7c1a
phantekzy <mainilotfi@gmail.com>
2026-02-10 13:56:03 +0100

    added viewPasswords

--- VaultManager.java
+++ VaultManager.java
@@ -6,4 +6,21 @@ public class VaultManager {
   public void addEntry(PasswordEntry entry) {
     vault.add(entry);
   }
+
+  public void viewPasswords() {
+    if (vault.isEmpty()) {
+      System.out.println("\n[!] Your vault is empty.");
+    } else {
+      System.out.println("\n--- YOUR SECURE PASSWORDS ---");
+      for (PasswordEntry p : vault) {
+        System.out.println(
+            "Site: "
+                + p.getWebsite()
+                + " | User: "
+                + p.getUsername()
+                + " | Pass: "
+                + p.getPassword());
+      }
+    }
+  }
 }
